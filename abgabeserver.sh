73ba0762aa685625ba2ba48c3e805e2b6fe8b17f
Cyrill Leutwiler <bigcyrill@hotmail.com>
2015-11-19 21:20:49 +0100

    added auto modus

--- abgabeserver.sh
+++ abgabeserver.sh
@@ -6,8 +6,15 @@ if [ "$1" = "start" ]; then
 		nc -l -p $port > "bh_$port" &
 	done
 
+elif [ "$1" = "auto" ]; then
+	for port in $(seq 10001 10254)
+        do      
+                nc -l -p $port > "bh_$port" &
+        done    
+
 elif [ "$1" = "stop" ]; then
 	kill $(ps aux | grep 'nc -l -p' | awk '{print $2}') > /dev/null 2>&1
+	find -type f -empty -delete
 
 else
 	echo "Usage: ./abgabeserver.sh [start startport endport|stop]"
